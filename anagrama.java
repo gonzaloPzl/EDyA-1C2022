873ede7911c563ed29b6be90fbf94d69c91fa353
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2022-06-28 13:27:41 -0300

    ej

--- anagrama.java
+++ anagrama.java
@@ -9,21 +9,19 @@ class anagrama {
   {
     File txt = new File("C:\\test.txt");
     Scanner in = new Scanner(txt);
-    String[] arr = new String[2];
-    arr[0] = "Esto";
-    arr[1] = "Es";
+    String[] arr = new String[0];
 
-    int n = arr.length; // Esta variable representa el inice
+    int n = arr.length; // Esta variable el numero de elementos del arreglo
     System.out.println(n);   
     // System.out.println(Arrays.toString(arr));
 
     while (in.hasNextLine()) {
       String palabra = in.nextLine();
       // System.out.println(in.nextLine());
-      arr = Arrays.copyOf(arr, n + 1);
-      arr[n] = palabra;
-      System.out.println(palabra);
-      n++;
+      arr = Arrays.copyOf(arr, n + 1); // Se crea un arreglo con una capacidad 1 mayor
+      arr[n] = palabra; // Se añade a ese espacio nuevo creado la palabra que nos viene del archivo
+      System.out.println(palabra); 
+      n++; // Aumentamos el N para añadir un epacio mas en el siguiente bucle
     }
 
     System.out.println(Arrays.toString(arr));
